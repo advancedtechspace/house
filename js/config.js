81e81e427b79776f958bbd8b19659a7d3022681e
sairessy <sairessy@gmail.com>
2025-03-18 01:31:06 +0200

    Responsive

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 const local_api = "http://127.0.0.1:8000";
 const remote_api = "https://api.advancedtechspace.com";
 
-const api_url = window.location.protocol === "https:" ? remote_api : local_api;
+const api_url = window.location.protocol !== "https:" ? remote_api : local_api;
 
 const title = "HOUSE";
 
